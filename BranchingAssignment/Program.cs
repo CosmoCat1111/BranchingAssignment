88666eef4ebbc203a6d9c8406161622fde745d90
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-10 00:05:30 -0700

    Add project files.

---
+++ BranchingAssignment/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setting max units 
+            int weightBox = 50;
+            int heightBox = 50;
+            int lengthBox = 50;
+            int widthBox = 50;
+
+            //welcome message
+            Console.WriteLine("Welcome to Package Express.  Please follow the instructions below.");
+
+            //user prompted to enter weight
+            Console.WriteLine("Please enter the weight: ");
+            int weightPackage = Convert.ToInt32(Console.ReadLine());
+
+            //if the weight is greater than 50, display error message. end program.
+            if (weightPackage > weightBox)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+
+            //if weight is good, proceed with entering other measurements
+            else if (weightPackage < weightBox)
+            {
+
+                //user prompted to enter width
+                Console.WriteLine("Please enter the width: ");
+                int widthPackage = Convert.ToInt32(Console.ReadLine());
+
+                //user prompted to enter height
+                Console.WriteLine("Please enter the height: ");
+                int heightPackage = Convert.ToInt32(Console.ReadLine());
+
+                //user promoted to enter length
+                Console.WriteLine("Please enter the length: ");
+                int lengthPackage = Convert.ToInt32(Console.ReadLine());
+
+
+                //creating box results and converting to currency. 
+                Console.WriteLine("You're estmiated shipping cost is listed below:");
+                int boxResult1 = (((widthPackage * heightPackage) * lengthPackage) * weightBox / 100);
+                Decimal boxResults1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("$" + boxResult1);
+                Console.ReadLine();
+            }
+
+         
+
+
+
+
+            //{
+            //    Console.WriteLine("Your package is too large to ship with Package Express, have a nice day!");
+            //    Console.ReadLine();
+            //}
+
+        }
+    }
+}
